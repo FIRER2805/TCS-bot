b1240435a8e1f96c49fa3d511a8ab86b1f3fa297
Gabriel Henrique Muller de Medeiros <gabriel_henrique2805@hotmail.com>
2024-06-22 13:54:01 -0300

    feat: criação de sessão no bot via endpoint

--- index.js
+++ index.js
@@ -3,17 +3,37 @@ const app = require("express")();
 const bodyParser = require("body-parser");
 const webhooks = require("node-webhooks");
 const Bot = require("./bot");
+const path = require('path');
+const fs = require('fs');
 
 bot = {};
 const PORT = 9000;
 
 app.use(bodyParser.json());
 
+app.get("/criar-sessao", async (req, res) => {
+    let bot = new Bot();
+    bot.criarSessao("sessão-teste");
+    const filePath = path.join(__dirname, "out.png");
+    const interval = setInterval(() => {
+        if (fs.existsSync(filePath)) {
+            clearInterval(interval); 
+
+            res.sendFile(filePath, (err) => {
+                if (err) {
+                    console.error('Erro ao enviar o arquivo:', err);
+                    res.status(500).send('Erro ao enviar o arquivo');
+                } else {
+                    console.log('Arquivo enviado com sucesso.');
+                }
+            });
+        }
+    }, 1000);
+});
+
 app.listen(PORT, (err)=>{
     if(!err){
         console.log("Servidor ouvindo na porta " + PORT);
-        bot = new Bot();
-        bot.criarSessao("sessão-teste");
     }
     else {
         console.log("Erro: " + err);
